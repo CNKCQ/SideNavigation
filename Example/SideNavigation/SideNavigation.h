e2d309a242d53b89ae769938de1df9a309694da0
Steve <chengquan.wang@ele.me>
2017-11-22 20:52:15 +0800

    for carthage

---
+++ Example/SideNavigation/SideNavigation.h
@@ -0,0 +1,19 @@
+//
+//  SideNavigation.h
+//  SideNavigation
+//
+//  Created by Steve on 22/11/2017.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SideNavigation.
+FOUNDATION_EXPORT double SideNavigationVersionNumber;
+
+//! Project version string for SideNavigation.
+FOUNDATION_EXPORT const unsigned char SideNavigationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SideNavigation/PublicHeader.h>
+
+
